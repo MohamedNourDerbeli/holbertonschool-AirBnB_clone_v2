ff808dbf219fa6a826dd42ca6be339bbaef69f99
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-12 11:40:07 +0100

    fix error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Create a Flask application instance"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+"""
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Returns the list of all States in the database."""
-    states = sorted(storage.all("State").values(), key=lambda s: s.name)
-    return render_template("7-states_list.html", states=states)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
@@ -19,7 +21,5 @@ def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    # Run the Flask application
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
