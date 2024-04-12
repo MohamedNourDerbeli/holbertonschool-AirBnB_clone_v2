93ff7ed50fdbe6264a4ca338febabdf51cdc81dd
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-12 11:51:54 +0100

    fix error

--- models/city.py
+++ models/city.py
@@ -6,8 +6,10 @@ from models.base_model import Base
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class City(BaseModel, Base):
     """The city class, contains state ID and name"""
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
