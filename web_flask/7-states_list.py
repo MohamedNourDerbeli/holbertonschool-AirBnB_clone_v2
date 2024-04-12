2af2c51d08f5ad54603cf1047567a2ba703c79ea
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-12 11:48:51 +0100

    fix error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Returns the list of all States in the database."""
-    states = storage.all("State")
+    states = sorted(storage.all("State").values(), key=lambda s: s.name)
     return render_template("7-states_list.html", states=states)
 
 
