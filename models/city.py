2690f8ba530902ac68cc8b3d605628b2af2e1ff9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-12 12:24:09 +0100

    fix error

--- models/city.py
+++ models/city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, ForeignKey, String
-from models.base_model import Base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class City(BaseModel, Base):
     """The city class, contains state ID and name"""
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "cities"
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities", cascade="delete")
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
